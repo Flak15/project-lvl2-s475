4b90eab5be48b389b2cad32919ac63b9df340565
flak15 <flak160@gmail.com>
2019-05-07 15:57:18 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import program from 'commander';
+import fs from 'fs';
+import _ from 'lodash';
 
-export default () => program
+export default (firstConfig, secondConfig) => {
+  program
   .description('Compare two configuration files and show a difference')
   .usage('[options] <firstConfig> <secondConfig>')
   .version('0.0.1')
   .option('-f, --format [type]', 'output format');
+
+  const generateJSONdifference = (firstConfig, secondConfig) => {
+    const beforeJSON = JSON.parse(firstConfig);
+    const afterJSON = JSON.parse(secondConfig);
+    const mergedKeys = _.union(_.keys(beforeJSON), _.keys(afterJSON));
+    const differenceJSON = mergedKeys.reduce( (acc, key) => {
+      const beforeJSONkeyValue = beforeJSON.hasOwnProperty(key) ? `- ${key}: ${beforeJSON[key]}\n` : '';
+      const afterJSONkeyValue = afterJSON.hasOwnProperty(key) ? `+ ${key}: ${afterJSON[key]}\n` : '';
+      if (beforeJSON[key] === afterJSON[key]) return `${acc}  ${key}: ${beforeJSON[key]}\n`
+      else return acc + beforeJSONkeyValue + afterJSONkeyValue;
+    }, '');
+    return `{\n${differenceJSON}}`;
+  }
+  return generateJSONdifference(firstConfig, secondConfig);
+}
