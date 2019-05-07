2484628a87a287f422e94e5b8dca3d9b1876d736
flak15 <flak160@gmail.com>
2019-05-07 16:24:12 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import program from 'commander';
-import fs from 'fs';
+//  import fs from 'fs';
 import _ from 'lodash';
 
 export default (firstConfig, secondConfig) => {
   program
-  .description('Compare two configuration files and show a difference')
-  .usage('[options] <firstConfig> <secondConfig>')
-  .version('0.0.1')
-  .option('-f, --format [type]', 'output format');
+    .description('Compare two configuration files and show a difference')
+    .usage('[options] <firstConfig> <secondConfig>')
+    .version('0.0.1')
+    .option('-f, --format [type]', 'output format');
 
-  const generateJSONdifference = (firstConfig, secondConfig) => {
-    const beforeJSON = JSON.parse(firstConfig);
-    const afterJSON = JSON.parse(secondConfig);
+  const generateJSONdifference = (firstJSON, secondJSON) => {
+    const beforeJSON = JSON.parse(firstJSON);
+    const afterJSON = JSON.parse(secondJSON);
     const mergedKeys = _.union(_.keys(beforeJSON), _.keys(afterJSON));
-    const differenceJSON = mergedKeys.reduce( (acc, key) => {
-      const beforeJSONkeyValue = beforeJSON.hasOwnProperty(key) ? `- ${key}: ${beforeJSON[key]}\n` : '';
-      const afterJSONkeyValue = afterJSON.hasOwnProperty(key) ? `+ ${key}: ${afterJSON[key]}\n` : '';
-      if (beforeJSON[key] === afterJSON[key]) return `${acc}  ${key}: ${beforeJSON[key]}\n`
-      else return acc + beforeJSONkeyValue + afterJSONkeyValue;
+    const differenceJSON = mergedKeys.reduce((acc, key) => {
+      const beforeJSONkeyValue = (key in beforeJSON) ? `- ${key}: ${beforeJSON[key]}\n` : '';
+      const afterJSONkeyValue = (key in afterJSON) ? `+ ${key}: ${afterJSON[key]}\n` : '';
+      if (beforeJSON[key] === afterJSON[key]) return `${acc}  ${key}: ${beforeJSON[key]}\n`;
+      return acc + beforeJSONkeyValue + afterJSONkeyValue;
     }, '');
     return `{\n${differenceJSON}}`;
-  }
+  };
   return generateJSONdifference(firstConfig, secondConfig);
-}
+};
