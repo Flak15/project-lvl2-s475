fefc2080955137f9d1a706c65c727b62ad21a9f3
flak15 <flak160@gmail.com>
2019-05-07 08:27:22 +0300

    add_asciinema

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,15 +3,8 @@
 import program from 'commander';
 
 program
-  .version('0.1.0')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .description('Compare two configuration files and show a difference')
+  .usage('[options] <firstConfig> <secondConfig>')
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
-
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
