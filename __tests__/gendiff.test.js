8e288bd40d5dd773505ccd2cbd4704dac1146479
flak15 <flak160@gmail.com>
2019-05-08 20:11:15 +0300

    changes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,18 +1,11 @@
 import fs from 'fs';
 import genDiff from '..';
+import _ from 'lodash';
 
-const json1 = fs.readFileSync('./__tests__/__fixtures__/firstConf.json');
-const json2 = fs.readFileSync('./__tests__/__fixtures__/secondConf.json');
-
-const result = `{
-  host: hexlet.io
-- timeout: 50
-+ timeout: 20
-- proxy: 123.234.53.22
-- follow: false
-+ verbose: true
-}`;
-
+const firstFilePath = '__tests__/__fixtures__/firstConf.json';
+const secondFilePath = '__tests__/__fixtures__/secondConf.json';
+const result = _.trim(fs.readFileSync('__tests__/__fixtures__/result1', 'utf-8'));
+const answ = genDiff(firstFilePath, secondFilePath);
 test('genDiff test 1', () => {
-  expect(genDiff(json1, json2)).toBe(result);
+  expect(answ).toBe(result);
 });
