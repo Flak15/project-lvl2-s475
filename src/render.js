85d334017896ca13245c5f73a8b67209b5453942
flak15 <flak160@gmail.com>
2019-05-16 16:07:26 +0300

    changes

--- src/render.js
+++ src/render.js
@@ -4,15 +4,15 @@ import stringify from './stringify';
 const render = (difference) => {
   const diffStringArray = difference.reduce((result, diffNode) => {
     if (diffNode.hasChildren()) {
-      return [...result, `  ${diffNode.getKey()}: ${render(diffNode.getChildren()).split('\n').join('\n\t')}`];
+      return [...result, `    ${diffNode.getKey()}: ${render(diffNode.getChildren()).split('\n').join('\n    ')}`];
     }
     if (diffNode.getMinusValue() === diffNode.getPlusValue()) {
-      return [...result, `  ${diffNode.getKey()}: ${diffNode.getMinusValue()}`];
+      return [...result, `    ${diffNode.getKey()}: ${diffNode.getMinusValue()}`];
     }
     const minusValue = diffNode.hasMinusValue() ? stringify(diffNode.getMinusValue()) : '';
     const plusValue = _.isObject(diffNode.getPlusValue()) ? stringify(diffNode.getPlusValue()) : diffNode.getPlusValue();
-    const minusString = diffNode.hasMinusValue() ? `- ${diffNode.getKey()}: ${minusValue}` : '';
-    const plusString = diffNode.hasPlusValue() ? `+ ${diffNode.getKey()}: ${plusValue}` : '';
+    const minusString = diffNode.hasMinusValue() ? `  - ${diffNode.getKey()}: ${minusValue}` : '';
+    const plusString = diffNode.hasPlusValue() ? `  + ${diffNode.getKey()}: ${plusValue}` : '';
     return [...result, `${_.isObject(minusString) ? stringify(minusString) : minusString}`, `${plusString}`];
   }, []);
   return `{\n${_.compact(diffStringArray).join('\n')}\n}`;
--- src/stringify.js
+++ src/stringify.js
@@ -1,12 +1,28 @@
 import _ from 'lodash';
-
+/*
 export default (value) => {
   if (_.isArray(value)) {
-    return `[\n\t${value.join(',\n\t')}\n  ]`;
+    return `[\n      ${value.join(',\n      ')}\n    ]`;
   }
   if (_.isPlainObject(value)) {
     return `{
-      ${Object.keys(value).map(key => `${key}: ${value[key]}\n`).join('\t')}  }`;
+        ${Object.keys(value).map(key => `${key}: ${value[key]}\n`).join('      ')}    }`;
   }
   return value;
 };
+*/
+const stringify = (value) => {
+  if (_.isArray(value)) {
+    return `[
+      ${value.join(',\n      ')}
+    ]`;
+  }
+  if (_.isPlainObject(value)) {
+  const objectString = `${Object.keys(value).map(key => `  ${key}: ${stringify(value[key])}\n`).join('  ')}`;
+  return `{
+      ${objectString.split('\n').slice(0, -1).join('\n    ')}
+    }`;
+  }
+  return value;
+};
+export default stringify;
