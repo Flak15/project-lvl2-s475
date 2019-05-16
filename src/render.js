3bc8ff48910200aba454bf3e6868354acdb27a27
flak15 <flak160@gmail.com>
2019-05-16 16:30:55 +0300

    changes

--- src/render.js
+++ src/render.js
@@ -2,19 +2,22 @@ import _ from 'lodash';
 import stringify from './stringify';
 
 const render = (difference) => {
-  const diffStringArray = difference.reduce((result, diffNode) => {
-    if (diffNode.hasChildren()) {
-      return [...result, `    ${diffNode.getKey()}: ${render(diffNode.getChildren()).split('\n').join('\n    ')}`];
-    }
-    if (diffNode.getMinusValue() === diffNode.getPlusValue()) {
-      return [...result, `    ${diffNode.getKey()}: ${diffNode.getMinusValue()}`];
-    }
-    const minusValue = diffNode.hasMinusValue() ? stringify(diffNode.getMinusValue()) : '';
-    const plusValue = _.isObject(diffNode.getPlusValue()) ? stringify(diffNode.getPlusValue()) : diffNode.getPlusValue();
-    const minusString = diffNode.hasMinusValue() ? `  - ${diffNode.getKey()}: ${minusValue}` : '';
-    const plusString = diffNode.hasPlusValue() ? `  + ${diffNode.getKey()}: ${plusValue}` : '';
-    return [...result, `${_.isObject(minusString) ? stringify(minusString) : minusString}`, `${plusString}`];
-  }, []);
-  return `{\n${_.compact(diffStringArray).join('\n')}\n}`;
+  const result = difference.reduce((result, diffNode) => {
+     if (diffNode.hasChildren()) {
+       const children = diffNode.getChildren();
+       return [...result, `  ${diffNode.getKey()}: ${render(children).split('\n').slice(0, -1).join('\n    ')}
+   }`];
+     }
+     if (diffNode.getMinusValue() === diffNode.getPlusValue())
+       return [...result, `  ${diffNode.getKey()}: ${diffNode.getMinusValue()}`];
+     const minusValue = stringify(diffNode.getMinusValue());
+     const plusValue = stringify(diffNode.getPlusValue());
+     const minusString = diffNode.hasMinusValue() ? `- ${diffNode.getKey()}: ${minusValue}` : '';
+     const plusString = diffNode.hasPlusValue() ? `+ ${diffNode.getKey()}: ${plusValue}` : '';
+     return [...result, minusString, plusString];
+ }, []);
+ return `{
+ ${_.compact(result).join('\n  ')}
+}`;
 };
 export default render;
