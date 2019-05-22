da09450d46e6e9129c802194cf5d2837e6850d53
flak15 <flak160@gmail.com>
2019-05-22 19:25:18 +0300

    refactoring_jsonRender

--- src/formatters/jsonFormatter.js
+++ src/formatters/jsonFormatter.js
@@ -1,17 +1,21 @@
 import _ from 'lodash';
 
 const renderInJsonFormat = (differenceAst) => {
-
   const renderProcessor = {
-    nested: diffNode => { prop: diffNode.property, innerJsonDiff: renderInJsonFormat(diffNode.children) },
-    added: diffNode => { prop: diffNode.property, finalValue: diffNode.finalValue },
-    removed: diffNode => { prop: diffNode.property, initValue: diffNode.initialValue },
-    changed: diffNode => { prop: diffNode.property, finalValue: diffNode.finalValue, initValue: diffNode.initialValue },
+    nested: diffNode => ({
+      prop: diffNode.property,
+      innerJsonDiff: renderInJsonFormat(diffNode.children),
+    }),
+    added: diffNode => ({ prop: diffNode.property, finalValue: diffNode.finalValue }),
+    removed: diffNode => ({ prop: diffNode.property, initValue: diffNode.initialValue }),
+    changed: diffNode => ({
+      prop: diffNode.property,
+      initValue: diffNode.initialValue,
+      finalValue: diffNode.finalValue,
+    }),
     unchanged: () => '',
-  }
-  const diffs = differenceAst.map((diffNode) => {
-    return renderProcessor[diffNode.type](diffNode);
-  });
+  };
+  const diffs = differenceAst.map(diffNode => renderProcessor[diffNode.type](diffNode));
   return _.compact(diffs);
 };
 
