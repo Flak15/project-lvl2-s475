684d4d96e5f8cdb76c5ffeeda591c19d46344500
flak15 <flak160@gmail.com>
2019-05-22 19:10:04 +0300

    sync

--- src/formatters/jsonFormatter.js
+++ src/formatters/jsonFormatter.js
@@ -1,33 +1,16 @@
 import _ from 'lodash';
 
 const renderInJsonFormat = (differenceAst) => {
+
+  const renderProcessor = {
+    nested: diffNode => { prop: diffNode.property, innerJsonDiff: renderInJsonFormat(diffNode.children) },
+    added: diffNode => { prop: diffNode.property, finalValue: diffNode.finalValue },
+    removed: diffNode => { prop: diffNode.property, initValue: diffNode.initialValue },
+    changed: diffNode => { prop: diffNode.property, finalValue: diffNode.finalValue, initValue: diffNode.initialValue },
+    unchanged: () => '',
+  }
   const diffs = differenceAst.map((diffNode) => {
-    if (diffNode.type === 'nested') {
-      return {
-        prop: diffNode.property,
-        innerJsonDiff: renderInJsonFormat(diffNode.children),
-      };
-    }
-    if (diffNode.type === 'added') {
-      return {
-        prop: diffNode.property,
-        finalValue: diffNode.finalValue,
-      };
-    }
-    if (diffNode.type === 'removed') {
-      return {
-        prop: diffNode.property,
-        initValue: diffNode.initialValue,
-      };
-    }
-    if (diffNode.type === 'changed') {
-      return {
-        prop: diffNode.property,
-        initValue: diffNode.initialValue,
-        finalValue: diffNode.finalValue,
-      };
-    }
-    return '';
+    return renderProcessor[diffNode.type](diffNode);
   });
   return _.compact(diffs);
 };
