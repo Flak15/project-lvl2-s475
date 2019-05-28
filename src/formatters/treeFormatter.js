742317d1f8fd5ce2cad6bd3b69e42ac5f3cc7385
flak15 <flak160@gmail.com>
2019-05-28 08:14:47 +0300

    treeFormatter_string_actions

--- src/formatters/treeFormatter.js
+++ src/formatters/treeFormatter.js
@@ -17,17 +17,14 @@ const stringify = (value) => {
 
 const render = (differenceAst) => {
   const renderProcessor = {
-    nested: diffNode => `  ${diffNode.property}: ${render(diffNode.children).split('\n').slice(0, -1).join('\n    ')}
-    }`,
+    nested: diffNode => `  ${diffNode.property}: ${render(diffNode.children).split('\n').slice(0, -1).join('\n    ')}\n    }`,
     unchanged: diffNode => `  ${diffNode.property}: ${stringify(diffNode.initialValue)}`,
     changed: diffNode => [`- ${diffNode.property}: ${stringify(diffNode.initialValue)}`, `+ ${diffNode.property}: ${stringify(diffNode.finalValue)}`],
     removed: diffNode => `- ${diffNode.property}: ${stringify(diffNode.initialValue)}`,
     added: diffNode => `+ ${diffNode.property}: ${stringify(diffNode.finalValue)}`,
   };
   const result = differenceAst.map(diffNode => renderProcessor[diffNode.type](diffNode));
-  return `{
-  ${_.compact(_.flatten(result)).join('\n  ')}
-}`;
+  return `{\n  ${_.compact(_.flatten(result)).join('\n  ')}\n}`;
 };
 
 export default render;
