577aaaae536e98370684fb72edd4ed12f6503201
flak15 <flak160@gmail.com>
2019-05-28 19:12:08 +0300

    f

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,10 @@
 import _ from 'lodash';
 import fs from 'fs';
 import genDiff from '..';
+import path from 'path';
 
+const fixuturesPath = path.join(__dirname, '__fixtures__');
+console.log(fixuturesPath);
 const result = _.trim(fs.readFileSync('__tests__/__fixtures__/result', 'utf-8'));
 const deepDiffResult = _.trim(fs.readFileSync('__tests__/__fixtures__/deepResult', 'utf-8'));
 const plainFormatResult = _.trim(fs.readFileSync('__tests__/__fixtures__/plainFormatResult', 'utf-8'));
