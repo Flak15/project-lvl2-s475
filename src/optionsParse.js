434c7931413705193ae3939f3d8a376aec087ea0
flak15 <flak160@gmail.com>
2019-05-29 08:17:19 +0300

    call_lib_from_optionParse-action.

--- src/optionsParse.js
+++ src/optionsParse.js
@@ -1,10 +1,14 @@
 import program from 'commander';
+import genDiff from '.';
 
 export default () => {
   program
     .description('Compare two configuration files and show a difference')
     .usage('[options] <firstConfig> <secondConfig>')
     .version('0.0.1')
-    .option('-f, --format [type]', 'output format');
+    .option('-f, --format [type]', 'output format')
+    .action((firstConfig, secondConfig, options) => {
+      console.log(genDiff(firstConfig, secondConfig, options.format));
+    });
   return program;
 };
