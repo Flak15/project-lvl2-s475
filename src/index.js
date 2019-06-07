7426a20ca9a2aaa50482802556fc891decd53542
flak15 <flak160@gmail.com>
2019-06-07 12:10:54 +0300

    parser_abstraction_fix

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import parse from './parsers';
 import genDifferenceAst from './genDiffAst';
 import render from './formatters/';
 
+const getDataType = ext => ext.slice(1);
+
 export default (firstFilePath, secondFilePath, format) => {
   const firstFileExtension = path.extname(firstFilePath);
   const secondFileExtension = path.extname(secondFilePath);
-  const firstDataObject = parse(fs.readFileSync(firstFilePath, 'utf-8'), firstFileExtension);
-  const secondDataObject = parse(fs.readFileSync(secondFilePath, 'utf-8'), secondFileExtension);
+  const firstDataObject = parse(fs.readFileSync(firstFilePath, 'utf-8'), getDataType(firstFileExtension));
+  const secondDataObject = parse(fs.readFileSync(secondFilePath, 'utf-8'), getDataType(secondFileExtension));
   const differenceAst = genDifferenceAst(firstDataObject, secondDataObject);
   return render(differenceAst, format);
 };
